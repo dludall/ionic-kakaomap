2c148dc0df033d203d61409981c777ab5f6366a7
wonsork <wonsork@gmail.com>
2023-12-13 10:22:02 +0900

    카카오 클러스터러 샘플
    카카오 클러스터러 샘플

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { CapacitorHttp, HttpResponse } from '@capacitor/core';
 import { Platform } from '@ionic/angular';
 
 declare let kakao: any;
@@ -12,11 +14,14 @@ declare let kakao: any;
 export class HomePage implements OnInit{
 
   map: any;
-  
-  constructor(private platform: Platform,) {}
-  ngOnInit(): void {    
-    this.platform.ready().then(async () => {
-      console.log("##########platform.ready ");
+
+  chickenData:any={};
+
+  constructor(private platform: Platform,
+    ) {}
+  async ngOnInit() {    
+    await fetch("assets/chicken.json").then(res=> res.json()).then(json=>{
+      this.chickenData = json;
     });
   }
 
@@ -28,6 +33,25 @@ export class HomePage implements OnInit{
       level: 13, //지도의 레벨(확대, 축소 정도)
     };
     this.map =  new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    
+
+    // 마커 클러스터러를 생성합니다 
+    var clusterer = new kakao.maps.MarkerClusterer({
+      map: this.map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
+      averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
+      minLevel: 10 // 클러스터 할 최소 지도 레벨 
+    });
+
+    var markers = this.chickenData.positions.map((position: any)=>{
+      console.log(position);
+      return new kakao.maps.Marker({
+              position : new kakao.maps.LatLng(position.lat, position.lng)
+          });
+    });
+  //   console.log(markers.length);
+  // // // 클러스터러에 마커들을 추가합니다
+    clusterer.addMarkers(markers);
+
   }
 
 }
